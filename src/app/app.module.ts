0abd87082e46243d493f7f4c8ca86a606b9ae5d5
Megha Priyadarshini <meghapriyadarshini68@gmail.com>
2020-07-27 20:25:52 +0530

    Extract Header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { ScreenComponent } from './screen/screen.component';
 import { TicketListComponent } from './ticket-list/ticket-list.component';
 import { TicketFormComponent } from './ticket-form/ticket-form.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ScreenComponent,
     TicketListComponent,
-    TicketFormComponent
+    TicketFormComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,

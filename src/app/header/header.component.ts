3e64b1cbe4a175553ad159e2aa73cd213a8a10a5
Megha Priyadarshini <meghapriyadarshini68@gmail.com>
2020-08-11 21:25:59 +0530

    Make links relative in app header

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,20 +1,15 @@
-import {Component, OnInit, Input} from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
-
   @Input() showNewTicketButton = false;
+  @Input() baseUrl: string;
 
+  constructor() {}
 
-  constructor() {
-  }
-
-  ngOnInit(): void {
-
-  }
-
+  ngOnInit(): void {}
 }
